5414dfd089c88590c9c68c578b5c3a68560aedf3
0xKato <emtolihi@gmail.com>
2022-11-28 00:40:10 +0100

    Update Wallet.jsx

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,29 @@
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from 'ethereum-cryptography/utils';
+import { keccak256 } from "ethereum-cryptography/keccak";
+
+
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey, userSign, setUserSign, signHash}) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey); 
+    const address = toHex(secp.getPublicKey(privateKey));
+
+    const messageHash = "caa0540304ec126f737a65ee31d171f89f0b1f46759730759f2ac6fc9556ecdb";
+
+    const userSign = secp.sign(messageHash, privateKey, { recovered: true });
+    const signHash= toHex(await userSign);
+
+    setUserSign(userSign);
+    console.log(signHash);
+
+    //const recover = secp.recoverPublicKey("caa0540304ec126f737a65ee31d171f89f0b1f46759730759f2ac6fc9556ecdb", toHex(userSign), recoveryBit);
+    //console.log(await recover);
+
+
+
     setAddress(address);
     if (address) {
       const {
@@ -12,6 +33,7 @@ function Wallet({ address, setAddress, balance, setBalance }) {
     } else {
       setBalance(0);
     }
+
   }
 
   return (
@@ -19,9 +41,10 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type in your Private Key" value={privateKey} onChange={onChange}></input>
       </label>
+      <div>address: {address.slice(0,5)}</div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
